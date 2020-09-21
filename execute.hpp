b8ccf5828feb27c07ec7a4a4e1d1b397429ca8b3
Ramasai Tadepalli <ramasai@terpmail.umd.edu>
2020-09-20 21:07:15 -0700

    select: fleshed out a basic implementation of select

--- execute.hpp
+++ execute.hpp
@@ -1,10 +1,19 @@
 #ifndef EXECUTE
 #define EXECUTE
 
+#include <streambuf>
+
 #include "types.hpp"
 
 void execute_create(CreateStatement);
 void execute_insert(InsertStatement);
 void execute_select(SelectStatement);
+
+class db_row_buffer : public std::streambuf {
+ public:
+  db_row_buffer(char* start, size_t size) {
+    this->setg(start, start, start + size);
+  }
+};
 
 #endif
